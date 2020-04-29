b22634fbab22ace7e3158b875f44c2af57e957ee
irinazorij <irina.zorij@gmail.com>
2020-04-29 23:01:48 +0300

    for gitlab

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,21 @@
 class MicropostsController < ApplicationController
   def home
   end
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+   if @micropost.save
+     flash[:success] = "Micropost created!"
+     redirect_to root_url
+   else
+      @feed_items = []
+     render 'static_pages/home'
+   end
+  end
+
+  def destroy
+    @micropost.destroy
+    redirect_to root_url
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
-Rails.application.routes.draw do
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+Todo::Application.routes.draw do
+  resources :users  do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
 end
